73e8492a0dd1c8be23fe6916e9ea15463bae863d
nikhilp <srinivasar@technoflair.com>
2020-09-18 09:19:50 +0530

    PWUE-01

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BsDropdownModule.forRoot()],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
