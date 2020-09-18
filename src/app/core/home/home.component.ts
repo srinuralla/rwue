15e094bd83056c0f4aeedfa2f119914929476b1a
TECHNODC\Srinivasar <srinivasar@technoflair.com>
2020-09-18 20:12:00 +0530

    final commit

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -6,7 +6,7 @@ import recentEvents from './../../../assets/recentEventDetails.json';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   recentEvents = recentEvents;
   newsOne;
   newsTwo;
@@ -20,46 +20,7 @@ export class HomeComponent implements OnInit {
         "'Six year old UP girl becomes the youngest trekker to reach the Kangchenjunga Base Camp '",
       id: '2',
     },
-    {
-      news:
-        "'Indian Air Force to welcome it’s first ever Women Fighter Pilots soon'",
-      id: '1',
-    },
-    {
-      news: "'The Cyberwar Needs More Women on the Front Lines'",
-      id: '2',
-    },
-    {
-      news:
-        "'Six year old UP girl becomes the youngest trekker to reach the Kangchenjunga Base Camp '",
-      id: '2',
-    },
-    {
-      news:
-        "'Indian Air Force to welcome it’s first ever Women Fighter Pilots soon'",
-      id: '1',
-    },
-    {
-      news: "'The Cyberwar Needs More Women on the Front Lines'",
-      id: '1',
-    },
-    {
-      news: "'The Cyberwar Needs More Women on the Front Lines'",
-      id: '2',
-    },
   ];
 
   constructor() {}
-
-  ngOnInit(): void {
-    let i = 0;
-    (function repeat(times) {
-      if (++i > 5) return;
-      setTimeout(function () {
-        this.newsOne = this.news[Math.floor(Math.random() * 6) + 1];
-        this.newsTwo = this.news[Math.floor(Math.random() * 6) + 1];
-        repeat();
-      }, 5000);
-    })();
-  }
 }
