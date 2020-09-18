8b8ebf8c892102380e6f8a973094175f53a2a5fd
TECHNODC\Srinivasar <srinivasar@technoflair.com>
2020-09-18 15:50:03 +0530

    unemployement

---
+++ src/app/modules/unemployment/unemployement-home/unemployement-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unemployement-home',
+  templateUrl: './unemployement-home.component.html',
+  styleUrls: ['./unemployement-home.component.scss']
+})
+export class UnemployementHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/unemployment/unemployment.module.ts
+++ src/app/modules/unemployment/unemployment.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UnemploymentRoutingModule } from './unemployment-routing.module';
 import { UnemploymentComponent } from './unemployment.component';
+import { UnemployementHomeComponent } from './unemployement-home/unemployement-home.component';
 
 
 @NgModule({
-  declarations: [UnemploymentComponent],
+  declarations: [UnemploymentComponent, UnemployementHomeComponent],
   imports: [
     CommonModule,
     UnemploymentRoutingModule
