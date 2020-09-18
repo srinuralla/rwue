a31be2e45248587386055c36e0d91bd3c17917e3
nikhilp <srinivasar@technoflair.com>
2020-09-18 18:51:27 +0530

    changes

--- src/app/shared/components/event-details/event-details.component.ts
+++ src/app/shared/components/event-details/event-details.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss'],
 })
-export class EventDetailsComponent implements OnInit {
+export class EventDetailsComponent {
   @Input() event;
   @Input() eventDetails;
-  constructor() {}
 
-  ngOnInit(): void {}
+  modalRef: BsModalRef;
+  message: string;
+  constructor(
+    private modalService: BsModalService,
+    private toastrService: ToastrService
+  ) {}
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+  confirm(): void {
+    this.message = 'Confirmed!';
+    this.toastrService.success(this.message);
+    this.modalRef.hide();
+  }
+  decline(): void {
+    this.message = 'Declined!';
+    this.modalRef.hide();
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
+import { ToastrModule } from 'ngx-toastr';
 import { EventsDisplayComponent } from './components/events-display/events-display.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [EventDetailsComponent, EventsDisplayComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ModalModule, ToastrModule.forRoot()],
   exports: [EventDetailsComponent, EventsDisplayComponent],
+  providers: [BsModalService],
 })
 export class SharedModule {}
