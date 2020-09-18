fdd08167e039b8044ba53afba59e0cc35f6170d8
nikhilp <srinivasar@technoflair.com>
2020-09-18 19:17:55 +0530

    changes

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -8,8 +8,37 @@ import recentEvents from './../../../assets/recentEventDetails.json';
 })
 export class HomeComponent implements OnInit {
   recentEvents = recentEvents;
-
+  newsOne;
+  newsTwo;
   newsDetails = [
+    {
+      news: "'The Cyberwar Needs More Women on the Front Lines'",
+      id: '1',
+    },
+    {
+      news:
+        "'Six year old UP girl becomes the youngest trekker to reach the Kangchenjunga Base Camp '",
+      id: '2',
+    },
+    {
+      news:
+        "'Indian Air Force to welcome it’s first ever Women Fighter Pilots soon'",
+      id: '1',
+    },
+    {
+      news: "'The Cyberwar Needs More Women on the Front Lines'",
+      id: '2',
+    },
+    {
+      news:
+        "'Six year old UP girl becomes the youngest trekker to reach the Kangchenjunga Base Camp '",
+      id: '2',
+    },
+    {
+      news:
+        "'Indian Air Force to welcome it’s first ever Women Fighter Pilots soon'",
+      id: '1',
+    },
     {
       news: "'The Cyberwar Needs More Women on the Front Lines'",
       id: '1',
@@ -22,5 +51,15 @@ export class HomeComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let i = 0;
+    (function repeat(times) {
+      if (++i > 5) return;
+      setTimeout(function () {
+        this.newsOne = this.news[Math.floor(Math.random() * 6) + 1];
+        this.newsTwo = this.news[Math.floor(Math.random() * 6) + 1];
+        repeat();
+      }, 5000);
+    })();
+  }
 }
