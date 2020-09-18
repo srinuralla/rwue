f054ca78dcf50c0217e8bf35dc3d7fa5b1ac31e4
TECHNODC\Srinivasar <srinivasar@technoflair.com>
2020-09-18 20:46:38 +0530

    add event changes

--- src/app/modules/education/modules/child-education/child-education-routing.module.ts
+++ src/app/modules/education/modules/child-education/child-education-routing.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChildEducationComponent } from './child-education.component';
+import { CreateEventsComponent } from 'src/app/shared/components/create-events/create-events.component';
 
 const routes: Routes = [
   { path: '', component: ChildEducationComponent },
   {
     path: 'event/:id',
     component: ChildEventDetailsComponent,
+  },
+  {
+    path: 'addevent',
+    component: CreateEventsComponent,
   },
 ];
 
--- src/app/modules/education/modules/women-education/women-education-routing.module.ts
+++ src/app/modules/education/modules/women-education/women-education-routing.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WomenEducationComponent } from './women-education.component';
+import { CreateEventsComponent } from 'src/app/shared/components/create-events/create-events.component';
 
 const routes: Routes = [
   { path: '', component: WomenEducationComponent },
   {
     path: 'event/:id',
     component: WomenEventDetailsComponent,
+  },
+  {
+    path: 'addevent',
+    component: CreateEventsComponent,
   },
 ];
 
