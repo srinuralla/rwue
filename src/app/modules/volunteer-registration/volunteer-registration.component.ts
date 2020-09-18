594a811987a9c9f69c0f1b8e5de440b60d77345e
srinivasa <srinivasar@technoflair.com>
2020-09-18 14:45:36 +0530

    new login form added

--- src/app/modules/volunteer-registration/volunteer-registration.component.ts
+++ src/app/modules/volunteer-registration/volunteer-registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import users from './../../../assets/users.json';
 @Component({
   selector: 'app-volunteer-registration',
   templateUrl: './volunteer-registration.component.html',
@@ -11,16 +13,42 @@ export class VolunteerRegistrationComponent implements OnInit {
   registrationForm: FormGroup = new FormGroup({
     firstName: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
-    userName: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
     confirmPassword: new FormControl('', Validators.required),
     phoneNumber: new FormControl('', Validators.required),
-    gender: new FormControl('', Validators.required),
 
   });
-  constructor() { }
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  constructor(private toastr: ToastrService, private router: Router,) { }
 
   ngOnInit(): void {
   }
-
+  submit() {
+    console.log(this.loginForm.value);
+    let user = users.find(
+      (x) =>
+        x.username.toLowerCase() === this.loginForm.value.userName.toLowerCase()
+    );
+    if (user) {
+      if (user.password === this.loginForm.value.password) {
+        localStorage.setItem('rwue_token', 'token');
+        localStorage.setItem('user_name', this.loginForm.value.userName);
+        this.toastr.success('Login Successfully');
+        this.loginForm.reset();
+        //  Navigate  Dashboard
+      }
+    } else {
+      this.toastr.error(
+        'Please verify credential',
+        'Invalid Username/Password'
+      );
+    }
+  }
+  register() {
+    this.toastr.success('Registered Successfully');
+    this.router.navigate(['']);
+  }
 }
