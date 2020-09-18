8b4f5bec4129ad24fa281e3a63e9339e46c333ba
nikhilp <srinivasar@technoflair.com>
2020-09-18 14:21:03 +0530

    event changes

--- src/app/shared/components/events-display/events-display.component.ts
+++ src/app/shared/components/events-display/events-display.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-events-display',
   templateUrl: './events-display.component.html',
   styleUrls: ['./events-display.component.scss'],
 })
-export class EventsDisplayComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+export class EventsDisplayComponent {
+  @Input() eventList: any[];
+  constructor(private router: Router) {}
+  navigateClick(e) {
+    this.router.navigate(e.routerLink);
+  }
 }
